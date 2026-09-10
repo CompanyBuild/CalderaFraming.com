1f154063d7e851302e943f1f90436388150666ed
CompanyBuild <edwincalderameza@gmail.com>
2026-09-10 19:29:18 -0400

    Enhance image click functionality
    Add click event to image for opening download URL in new tab.

--- script.js
+++ script.js
@@ -103,7 +103,11 @@ async function loadProjects() {
             const image = document.createElement("img");
 
             image.src = file.download_url;
-
+            img.onclick = function() {
+                window.open(file.download_url, "_blank");
+            };
+            
+            img.style.cursor = "pointer";
             image.alt = getProjectName(file.name);
 
 
