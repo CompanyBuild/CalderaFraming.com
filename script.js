b66b7237405b149092123c92982ae65c4a554cf5
CompanyBuild <edwincalderameza@gmail.com>
2026-09-11 16:58:48 -0400

    Update project description formatting
    Replace newline characters in project description with HTML line breaks.

--- script.js
+++ script.js
@@ -185,9 +185,7 @@ async function loadProjects() {
 
             const description = document.createElement("p");
 
-            description.textContent =
-                info.description || "Previous project.";
-
+            description.textContent = info.description.replace(/\n/g, "<br>"); || "No desc";
 
             // Assemble card
 
